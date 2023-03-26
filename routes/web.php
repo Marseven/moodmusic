e01dfc1ba44d8b029a6b0e42dc126cfde6b14e9c
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-03-26 14:42:21 +0100

    Install Fortify

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//home
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+Route::get('/home', [WelcomeController::class, 'index']);
+
+Route::get('logout',  function () {
+    Auth::logout();
+
+    return redirect('home');
 });
+
+Route::get('503', function () {
+    return 'Accès non autorisé';
+});
+
+Route::get('404', function () {
+    return 'Page non trouvée';
+});
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/profil');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email/verify', function () {
+    return redirect('/profil')->with('error', "Vous devez verifier votre email pour accéder à cette page.");
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verification-notification', function () {
+    $user = User::find(auth()->user()->id);
+    $user->sendEmailVerificationNotification();
+
+    return back()->with('success', 'Le lien de vérification a été envoyé. Consultez votre boîte mail (les spams également) pour valider votre email.');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
